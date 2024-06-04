0c7032e98eacb7b353ee33f1d9872ab0e54a157a
Prabhdeep101 <prabhdeepsachdeva101@gmail.com>
2024-06-05 09:04:49 +1200

    commit

--- Rasai_web/wwwroot/js/site.js
+++ Rasai_web/wwwroot/js/site.js
@@ -1,4 +1,35 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿<script>
+    function myFunction() {
+        var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        } else {
+        x.style.display = "block";
+        }
+    }
+    /* dropdown menu functions */
+    document.addEventListener("DOMContentLoaded", function () {
+        var dropdown = document.querySelector('.dropdown');
+    var dropdownContent = document.querySelector('.dropdown-content');
+    var timer;
 
-// Write your JavaScript code.
+    dropdown.addEventListener('mouseenter', function () {
+        clearTimeout(timer); // Clear any existing timeout
+    dropdownContent.style.display = 'block'; // Show the dropdown menu
+        });
+
+    dropdown.addEventListener('mouseleave', function () {
+        timer = setTimeout(function () {
+            dropdownContent.style.display = 'none'; // Hide the dropdown menu after a delay
+        }, 1000); // Adjust delay (in milliseconds) as needed
+        });
+
+    dropdownContent.addEventListener('mouseenter', function () {
+        clearTimeout(timer); // Clear the timeout when mouse enters the dropdown menu
+        });
+
+    dropdownContent.addEventListener('mouseleave', function () {
+        dropdownContent.style.display = 'none'; // Hide the dropdown menu when mouse leaves
+        });
+    });
+</script>
