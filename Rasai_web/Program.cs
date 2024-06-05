7d7cd4bd57c2117b9194ed50e78841cfcc1dca24
Prabhdeep101 <prabhdeepsachdeva101@gmail.com>
2024-06-05 18:07:46 +1200

    database operations

--- Rasai_web/Program.cs
+++ Rasai_web/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Rasai_web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure the connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MenuContext>(options =>
+    options.UseSqlite(connectionString));
 
 var app = builder.Build();
 
@@ -9,7 +17,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
