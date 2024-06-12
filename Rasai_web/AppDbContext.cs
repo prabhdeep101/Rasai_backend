49ddbdb8bc50df14b4f321a06fd55047eeee44eb
Prabhdeep101 <prabhdeepsachdeva101@gmail.com>
2024-06-12 16:18:28 +1200

    program.cs changed for booking

---
+++ Rasai_web/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rasai_web.Models;
+
+namespace Rasai_web
+{
+
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+    }
+
+}
